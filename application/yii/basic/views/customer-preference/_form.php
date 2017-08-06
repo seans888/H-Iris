6c98176267342c885f52bee346e9336cc4938b56
Shella Mae Gutierrez <shellamaeg.smg@gmail.com>
2017-08-06 19:20:59 +0800

    removed unnecessary lines

--- application/yii/basic/views/customer-preference/_form.php
+++ application/yii/basic/views/customer-preference/_form.php
@@ -13,25 +13,10 @@ use app\models\Preference;
 
 <div class="customer-preference-form">
 
-    <?php $form = ActiveForm::begin(); ?>
-
-<<<<<<< HEAD
-    <?= $form->field($model, 'customer_id')->dropDownList( 
-            ArrayHelper::map(Customer::find()->all(),'id','name'),
-            ['prompt'=>'Select Customer'])
-           ?> 
-
-           <?= $form->field($model, 'preference_id')->dropDownList( 
-            ArrayHelper::map(Preference::find()->all(),'id','information'),
-            ['prompt'=>'Select Preference'])
-           ?> 
-=======
-
   <?php $form = ActiveForm::begin(); ?>
          <?= $form->field($model, 'customer_id')->dropDownList( 
             ArrayHelper::map(Customer::find()->all(),'id','name'),
             ['prompt'=>'Select Customer']
-
 
         ) ?> 
 
@@ -40,9 +25,7 @@ use app\models\Preference;
             ArrayHelper::map(Preference::find()->all(),'id','information'),
             ['prompt'=>'Select Preference']
 
-
         ) ?> 
->>>>>>> 141ebc8666a9f8e04304c85e35d048ed5cbca640
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
