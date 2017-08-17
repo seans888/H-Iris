5091a0da9eca8782b2eb1cadc0672e1b6221f2be
Shella Mae Gutierrez <shellamaeg.smg@gmail.com>
2017-08-17 13:14:28 +0800

    fixed all the format under spa and offers & activities menu tabs

--- application/yii/basic/views/website/roomoffer.php
+++ application/yii/basic/views/website/roomoffer.php
@@ -15,16 +15,13 @@ $this->params['breadcrumbs'][] = $this->title;
 
 <div>
 <h1> OFFERS AND ACTIVITIES </h1>
+<hr class = "hrColor">
 <h2>ROOM OFFERS</h2>
-
-</br>
-</br>
-
-<h2>Family Package</h2>
+<hr class = "hrColor">
+<h3>FAMILY PACKAGE</h3>
  <img src="uploads/roomoffer.jpg" style="width:40%;height:40%;"> 
  <div class="bullet">
     <p>
-<h3>Family Package </h3>
     	</br>
      <strong> Any day is always a great day for a family escapade at Pico Sands Hotel.</strong> </br>  </br>Book now and pay a starting rate of PHP 9,900 a night on our <strong> Premier Mountain View Room</strong>  </br> </br>or </br></br> PHP 10,900 a night on our <strong> Premier Lagoon View Room</strong>  using this special promo!
 </br>
