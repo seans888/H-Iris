20cd47f8d7b630d8785c337bf3186d66ceaaa77b
Shella Mae Gutierrez <shellamaeg.smg@gmail.com>
2017-08-17 10:36:45 +0800

    added color in the <hr> element in pre-wedding activities

--- application/yii/basic/views/website/prewedding.php
+++ application/yii/basic/views/website/prewedding.php
@@ -57,10 +57,10 @@ function showSlides() {
  
 <div>
 <h1> WEDDINGS</h1>
-<hr>
+<hr style="border-color:#4aa3ef">
 
  <h2>PRE-WEDDING ACTIVITIES</h2>
- <hr>
+ <hr style="border-color:#4aa3ef;">
 <h3>PRENUP PICTORIAL PACKAGE</h3>
 
 <h5>INCLUSIONS:</h5>
