de3fd2f24a9af4c78246b8f53fd5189f8d0a9a66
Shierene Cervantes <shierenecervantes23@gmail.com>
2017-08-27 11:23:13 +0800

    Set database to mi151_00_master_db

--- application/yii/basic/config/db.php
+++ application/yii/basic/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=crm',
+    'dsn' => 'mysql:host=localhost;dbname=mi151_00_master_db',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
