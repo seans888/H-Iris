7473b9611587bf80e7134b139327975d22b6e611
Shierene Cervantes <shierenecervantes23@gmail.com>
2017-09-01 21:01:28 +0800

    Tried sending an email using swift mailer

--- application/yii/backend/config/params.php
+++ application/yii/backend/config/params.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'adminEmail' => 'admin@example.com',
+    'adminEmail' => 'shierenecervantes23@gmail.com',
 ];
--- application/yii/backend/config/web.php
+++ application/yii/backend/config/web.php
@@ -21,14 +21,47 @@ $config = [
         ],
         'errorHandler' => [
             'errorAction' => 'site/error',
+        ], 
+      /*return [
+        'components' => [
+            'db' => [
+                'class' => 'yii\db\Connection',
+                'dsn' => 'mysql:host=localhost;dbname=mi151_00_master_db',
+                'username' => 'root',
+                'password' => 'as',
+                'charset' => 'utf8',
+                ],
+            'mailer' => [
+                'class' => 'yii\swiftmailer\Mailer',
+                'viewPath' => '@app/mail',
+                //'useFileTransport' => false,
+                'transport' => [
+                    'class' => 'Swift_SmtpTransport',
+                     'host'=>'smtp.gmail.com',
+                    'username'=>'***@gmail.com',
+                    'password'=>'***',
+                    'port' => '587',
+                    'encryption' => 'tls',
+                ],
+            ],
         ],
+    ];*/
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
-            'useFileTransport' => true,
+            'useFileTransport' => false,
+            'transport' => [
+                    'class' => 'Swift_SmtpTransport',
+                     'host'=>'smtp.gmail.com',
+                    'username'=>'shierenecervantes23@gmail.com',
+                    'password'=>'Password',
+                    'port' => '465',
+                    'encryption' => 'ssl',
         ],
+        ],
+       
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
             'targets' => [
@@ -50,6 +83,8 @@ $config = [
     ],
     'params' => $params,
 ];
+
+
 
 if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
