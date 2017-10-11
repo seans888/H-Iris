40354ba359c78fe74cfb37e3a808d120ed39d8ef
Mary Hazel Kate Comia <mtcomia@student.apc.edu.ph>
2017-10-11 17:18:13 +0800

    added the codes of the previous website to the advance- frontend

--- advanced/frontend/views/layouts/main.php
+++ advanced/frontend/views/layouts/main.php
@@ -1,44 +1,124 @@
 <?php
-
 /* @var $this \yii\web\View */
 /* @var $content string */
-
 use yii\helpers\Html;
 use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
 use frontend\assets\AppAsset;
 use common\widgets\Alert;
-
 AppAsset::register($this);
 ?>
-<?php $this->beginPage() ?>
-<!DOCTYPE html>
-<html lang="<?= Yii::$app->language ?>">
-<head>
-    <meta charset="<?= Yii::$app->charset ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?= Html::csrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
-</head>
-<body>
-<?php $this->beginBody() ?>
+    <?php $this->beginPage() ?>
+        <!DOCTYPE html>
+        <html lang="<?= Yii::$app->language ?>">
 
-<div class="wrap">
-    <?php
+        <head>
+            <meta charset="<?= Yii::$app->charset ?>">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <?= Html::csrfMetaTags() ?>
+                <title>
+                    <?= Html::encode($this->title) ?>
+                </title>
+                <?php $this->head() ?>
+        </head>
+
+        <body>
+            <?php $this->beginBody() ?>
+
+                <div class="wrap">
+                    <?php
     NavBar::begin([
-        'brandLabel' => 'My Company',
+        'brandLabel' => 'SM Hotels and Convention Center',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
         ],
     ]);
     $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
+ ['label' => 'Home', 'url' => ['/site/index']],
+ ['label' => 'About', 'url' => ['/site/about']],
+['label' => 'Room',
+ 'items' => [
+                 ['label' => 'Overview' , 'url' => ['website/roomoverview']],
+
+                 '<li class="dropdown-header"><a href="http://localhost/yii/web/index.php?r=website%2Flagoon">Lagoon View</a> </li>',
+                 '<li class="dropdown-header"> </li>',
+                 '<li class="dropdown-header"> </li>',
+                 ['label' => 'Superior Room', 'url' => ['/website/superior']],
+                 ['label' => 'Deluxe Room', 'url' => ['/website/deluxe']],
+                 ['label' => 'Premier Room', 'url' => ['/website/premier']],
+                 ['label' => 'Corner Deluxe Rooms', 'url' => ['/website/cornerdeluxe']],
+                 ['label' => 'Penthouse Loft Rooms', 'url' => ['/website/penthouse']],
+                '<li class="dropdown-header"> </li>',
+                  '<li class="dropdown-header"><a href="http://localhost/yii/web/index.php?r=website%2Fmountain">Mountain View</a> </li>',
+                 ['label' => 'Standard Room', 'url' => ['/website/standard']],
+                 ['label' => 'Superior Room', 'url' => ['/website/msuperior']],
+                 ['label' => 'Premier Room', 'url' => ['/website/mpremier']],
+            ],],
+['label' => 'Dining', 
+'items' => [
+                 ['label' => 'Pico Restaurant and Bar' , 'url' => ['/website/randb']],
+
+                 ['label' => 'Reef Bar', 'url' => ['/website/reefbar']],
+                 ['label' => 'B&B', 'url' => ['/website/bandb']],
+                 ['label' => 'Lagoa', 'url' => ['/website/lagoa']],
+                 ], ],
+
+ ['label' => 'Spa', 
+ 'items' => [
+                 ['label' => 'Overview' , 'url' => ['/website/spa']],
+                 ['label' => 'Booking Guidelines', 'url' => ['/website/spabookguide']],
+                 ], ],
+
+ ['label' => 'Offers & Activities',
+'items' => [
+                 ['label' => 'Room Offers' , 'url' => ['/website/roomoffer']],
+                 ['label' => 'Club Activities', 'url' => ['/website/clubactivity']],
+                  ['label' => 'Promotions', 'url' => ['/website/promotions']],
+                 ], ],
+ ['label' => 'Events', 
+  'items' => [
+
+                 '<li class="dropdown-header"> </li>',
+                 '<li class="dropdown-header">Weddings</li>',
+
+                 ['label' => 'Pre-Wedding Activities', 'url' => ['/website/prewedding']],
+
+                 ['label' => 'Wedding Packages', 'url' => ['/website/weddingpackage']],
+
+                 ['label' => 'Ceremony Venues', 'url' => ['/website/ceremonyvenue']],
+                 ['label' => 'Reception Venues', 'url' => ['/website/reception']],
+                '<li class="dropdown-header"> </li>',
+                 '<li class="dropdown-header">Corporate Meetings</li>',
+                   ['label' => 'Team building Activities', 'url' => ['/website/teambuild']],
+
+],
+],
+   ['label' => 'Gallery', 
+    'items' => [
+
+                  '<li class="dropdown-header"> </li>',
+                 '<li class="dropdown-header">Photos</li>',
+
+                 ['label' => 'Rooms', 'url' => ['/website/room']],
+
+                  ['label' => 'Dining', 'url' => ['/website/dining']],
+
+                  ['label' => 'Leisure', 'url' => ['/website/leisure']],
+
+                 ['label' => 'Events', 'url' => ['/website/event']],
+
+                 '<li class="dropdown-header"> </li>',
+                  '<li class="dropdown-header">Videos</li>',     
+
+ ],
+ ],
+   ['label' => 'Location', 'url' => ['/website/location']],
+
+ ['label' => 'Contact', 'url' => ['/site/contact']],
+
     ];
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
@@ -60,24 +140,29 @@ AppAsset::register($this);
     NavBar::end();
     ?>
 
-    <div class="container">
-        <?= Breadcrumbs::widget([
+                        <div class="container">
+                            <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
-    </div>
-</div>
+                                <?= Alert::widget() ?>
+                                    <?= $content ?>
+                        </div>
+                </div>
 
-<footer class="footer">
-    <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+                <footer class="footer">
+                    <div class="container">
+                        <p class="pull-left">&copy; My Company
+                            <?= date('Y') ?>
+                        </p>
 
-        <p class="pull-right"><?= Yii::powered() ?></p>
-    </div>
-</footer>
+                        <p class="pull-right">
+                            <?= Yii::powered() ?>
+                        </p>
+                    </div>
+                </footer>
 
-<?php $this->endBody() ?>
-</body>
-</html>
-<?php $this->endPage() ?>
+                <?php $this->endBody() ?>
+        </body>
+
+        </html>
+        <?php $this->endPage() ?>
--- advanced/frontend/views/site/about.php
+++ advanced/frontend/views/site/about.php
@@ -7,10 +7,142 @@ use yii\helpers\Html;
 $this->title = 'About';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+
 <div class="site-about">
+
+	<img src="uploads/sm.jpg" style="width:100px;height:100px;"> <br> 
+    <div class="body-content">
+        <div class="slideshow-container">
+
+  <div class="mySlides">
+    <img src="uploads/Taalhotel.jpg" style="width:1000px;height:500px"> 
+  </div>
+
+  <div class="mySlides">
+    <img src="uploads/Taalroom.jpg" style="width:1000px;height:500px"> 
+  </div>
+
+  <div class="mySlides">
+    <img src="uploads/Dining.jpg" style="width:1000px;height:500px;"> 
+  </div>
+ 
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span> 
+  <span class="dot" onclick="currentSlide(2)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span> 
+</div>
+<script>
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+  
+
+</script>
+
     <h1><?= Html::encode($this->title) ?></h1>
+<hr class = "hrColor">
 
-    <p>This is the About page. You may modify the following file to customize its content:</p>
+    <p>
 
-    <code><?= __FILE__ ?></code>
+    	<br>
+        SM Hotels and Conventions Corporation (SMHCC) was established to address the vast potential of tourism in the country. 
+    	</br>
+    	It is now developing and operating hotels and convention centers all throughout the archipelago with a current portfolio
+    	</br>
+    	of 1,514 rooms housed in the 261-room Taal Vista Hotel, a heritage hotel located in Tagaytay City; the 396-room upper
+    	<br>
+    	upscale Radisson Blu Hotel in Cebu; the 154-room Pico Sands Hotel in Hamilo Coast; the 202-room Park Inn by Radisson
+    	</br>
+    	in Davao; the 154-room Park Inn by Radisson in Clark in Pampanga; and the 347-guest room deluxe 5-star hotel,  
+    	<br>
+    	Conrad Manila, located in the Mall of Asia Complex.
+    	</br>
+ 
+    </p>
+
+    <p>
+    	<br>
+    	All these allow guests to experience luxury and the world-renowned Filipino hospitality, made more memorable by the
+    	</br>
+    	natural beauty of their surrounding landscapes.
+    </p>
+
+    <p>
+    	<br>
+    	SMHCC operates convention centers and trade halls through SMX Convention Center (SMX), which has become a popular 
+    	</br>
+   		venue for both local and international events. Setting the bar in upscale convention facilities, SMX provides an ideal venue
+   		<br>
+   		for large-scale institutional events, townhall meetings, weddings, exhibits, and concerts in many cities across the country.
+   		</br>
+    </p>
+
+    <p>
+    	<br>
+    	Consequently, SMX has branch out to Taguig, Davao and Bacolod. SMHCC also operates trade halls in SM Megamall and 
+    	</br>
+    	SM City Cebu. Put together, SMX has a total of 35,623 square meters of gross leasable area (GLA) making it the largest 
+    	<br>
+    	privately run exhibition and conventions business in the Philippines.
+    </p>
+  
+
+		<br> 
+        SM Hotels and Conventions Corporation (SMHCC) was established to address the vast potential of tourism in the country.  
+		</br> 
+		It is now developing and operating hotels and convention centers all throughout the archipelago with a current portfolio 
+		</br> 
+		of 1,514 rooms housed in the 261-room Taal Vista Hotel, a heritage hotel located in Tagaytay City; the 396-room upper 
+		<br> 
+		upscale Radisson Blu Hotel in Cebu; the 154-room Pico Sands Hotel in Hamilo Coast; the 202-room Park Inn by Radisson 
+		</br> 
+		in Davao; the 154-room Park Inn by Radisson in Clark in Pampanga; and the 347-guest room deluxe 5-star hotel,   
+		<br> 
+		Conrad Manila, located in the Mall of Asia Complex. 
+		</br> 
+    </p>
+    <p> 
+		<br> 
+		All these allow guests to experience luxury and the world-renowned Filipino hospitality, made more memorable by the 
+		</br> 
+		natural beauty of their surrounding landscapes. 
+	</p> 
+	<p> 
+		<br> 
+		SMHCC operates convention centers and trade halls through SMX Convention Center (SMX), which has become a popular  
+		</br> 
+		venue for both local and international events. Setting the bar in upscale convention facilities, SMX provides an ideal venue 
+		<br> 
+		for large-scale institutional events, townhall meetings, weddings, exhibits, and concerts in many cities across the country. 
+		</br> 
+	</p> 
+	<p> 
+		<br> 
+		Consequently, SMX has branch out to Taguig, Davao and Bacolod. SMHCC also operates trade halls in SM Megamall and  
+		</br> 
+		SM City Cebu. Put together, SMX has a total of 35,623 square meters of gross leasable area (GLA) making it the largest  
+		<br> 
+		privately run exhibition and conventions business in the Philippines. 
+	</p> 
+
 </div>
